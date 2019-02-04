85cdd5f3fdd35feb2f6e707fd15ec9850ad3329b
Ivaanbola <ivaanbola@gmail.com>
2019-02-04 20:04:55 +0100

    Gatitos con Hibernate

--- src/dao/DAOGatitos.java
+++ src/dao/DAOGatitos.java
@@ -59,3 +59,47 @@ public class DAOGatitos {
 	}
 
 }
+/*
+	public static List<Gatitos> BuscarPorNombre(String nombre) {
+		Session sesion = Conexion.getSesion().openSession();
+		sesion.beginTransaction();
+
+		Query q = sesion.createQuery("FROM Gatitos g WHERE g.nombre = :nombre");
+		q.setParameter("nombre", nombre);
+		List<Gatitos> result = q.list();
+
+		sesion.getTransaction().commit();
+		sesion.close();
+		return result;
+	} // mapeado
+
+	public static List<Gatitos> todosMapeados() {
+		Session sesion = Conexion.getSesion().openSession();
+		sesion.beginTransaction();
+		Query q = sesion.getNamedQuery("dameTodosPorRaza");
+		q.setParameter("raza", "British");
+		List<Gatitos> result = q.list();
+		sesion.getTransaction().commit();
+		sesion.close();
+		return result;
+	}
+
+	public static List<Gatitos> obtenerTodos() {
+		Session sesion = Conexion.getSesion().openSession();
+		sesion.beginTransaction();
+
+		Query q = sesion.createQuery("from Gatitos");
+		List<Gatitos> result = q.list();
+		sesion.getTransaction().commit();
+		sesion.close();
+		return result;
+	}
+
+	public static List<Gatitos> listar() { Session sesion =
+  Conexion.getSesion().openSession(); sesion.beginTransaction();
+  
+  List<Gatitos> result =
+  (List<Gatitos>)sesion.createCriteria(Gatitos.class).list(); //List<Gatitos>
+  result = sesion.createSQLQuery("from modelo.Gatitos as cat").list();
+  sesion.getTransaction().commit(); sesion.close(); return result; }
+*/
